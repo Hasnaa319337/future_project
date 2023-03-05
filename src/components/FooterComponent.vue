6f91bd11478ecb6da896f35b401db0d9e13f6e0f
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-03-05 23:41:02 +0200

    first commit

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+<footer>
+
+    <v-container>
+        footer
+    </v-container>
+</footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import FooterComponent from "@/components/FooterComponent.vue"
+</script>
+
+<template>
+  <main>
+   <footer-component/>
+  </main>
+</template>
