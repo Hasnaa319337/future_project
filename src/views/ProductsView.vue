02f488c2d123356f1503651bc705e7286058ab19
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-03-07 05:18:03 +0200

    products view

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <NavbarComponent />
+
+  <HeaderComponent />
+  <h3>products</h3>
+  <Footercomponent />
+</template>
+
+<script>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import Footercomponent from "@/components/FooterComponent.vue";
+// import MainButton from "@/components/ui/MainButton.vue";
+import NavbarComponent from "@/components/NavbarComponent.vue";
+export default {
+  components: {
+    HeaderComponent,
+    Footercomponent,
+    NavbarComponent,
+    // MainButton,
+  },
+};
+</script>
+
+<style></style>
